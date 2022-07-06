c9c7ef362c660e698527643b1e2a909808f867a9
Rohan Singh Gautam <69078232+Singhji014@users.noreply.github.com>
2022-07-07 00:05:11 +0530

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -8,40 +8,42 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+level  = input("what level of difficulty would you like ? Easy or Hard\n")
 
 #Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-# password = ""
-# for char in range(0, nr_letters):
-#     password += random.choice(letters)
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+if level == "Easy":    
+    passwordeasy = ""
+    for char in range(0, nr_letters):
+        passwordeasy += random.choice(letters)
 
-# for sym in range(0, nr_symbols):
-#     password += random.choice(symbols)
+    for sym in range(0, nr_symbols):
+        passwordeasy += random.choice(symbols)
 
-# for int in range(0, nr_numbers):
-#     password += random.choice(numbers)
+    for int in range(0, nr_numbers):
+        passwordeasy += random.choice(numbers)
 
-# print(password)
+    print(passwordeasy)
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+elif level == "Hard":
+    password_list = []
 
-password_list = []
+    for char in range(0, nr_letters):
+        password_list += random.choice(letters)
 
-for char in range(0, nr_letters):
-    password_list += random.choice(letters)
+    for sym in range(0, nr_symbols):
+        password_list += random.choice(symbols)
 
-for sym in range(0, nr_symbols):
-    password_list += random.choice(symbols)
-
-for int in range(0, nr_numbers):
-    password_list += random.choice(numbers)
+    for int in range(0, nr_numbers):
+        password_list += random.choice(numbers)
 
 
-random.shuffle(password_list)
+    random.shuffle(password_list)
 
-password = ""
-for char in password_list:
-    password += char
+    password = ""
+    for char in password_list:
+        password += char
 
-print(password)
+    print(password)
